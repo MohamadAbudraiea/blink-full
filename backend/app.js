05eed36bc33e1c7cc6204d3dd0be1d30b51951ff
Mohammad Abudraiea <mohamadabudraiea2003@outlook.com>
2026-06-15 11:32:22 +0300

    fix: change morgan logging format from 'dev' to 'common' in development mode

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const app = express();
 //-----------------------------------------
 app.use(express.json());
 app.use(cookieParser());
-if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+if (process.env.NODE_ENV === "development") app.use(morgan("common"));
 
 const allowedOrigins = [
   "https://blink-cars.com",
